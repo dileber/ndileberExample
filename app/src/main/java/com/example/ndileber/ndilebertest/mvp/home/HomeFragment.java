e79669b5b1f9382031b6ae9ab494db57c71c24e3
wangerdan <297165331@qq.com>
2017-10-10 09:48:14 +0800

    更新

--- app/src/main/java/com/example/ndileber/ndilebertest/mvp/home/HomeFragment.java
+++ app/src/main/java/com/example/ndileber/ndilebertest/mvp/home/HomeFragment.java
@@ -1,12 +1,20 @@
 package com.example.ndileber.ndilebertest.mvp.home;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.drcosu.ndileber.mvp.ubase.UBaseFragment;
+import com.drcosu.ndileber.tools.DialogLinstener;
+import com.drcosu.ndileber.tools.UDialog;
 import com.example.ndileber.ndilebertest.R;
 
 
-public class HomeFragment extends UBaseFragment<HomePresenter> implements HomeContract.View<HomePresenter> {
+public class HomeFragment extends UBaseFragment<HomePresenter> implements HomeContract.View<HomePresenter>,View.OnClickListener {
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,10 +32,6 @@ public class HomeFragment extends UBaseFragment<HomePresenter> implements HomeCo
         return false;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected HomePresenter createPresenterInstance() {
@@ -42,6 +46,81 @@ public class HomeFragment extends UBaseFragment<HomePresenter> implements HomeCo
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        showSplash();
+        setOnClickListener(this,R.id.button,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8);
+    }
 
+    class Splashhandler implements Runnable{
+
+        public void run() {
+            loadDialogDismiss();
+        }
+
+    }
+
+    Splashhandler splashhandler;
+    Handler x;
+    public void showSplash() {
+
+        x = new Handler();
+        splashhandler = new Splashhandler();
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Button temp = null;
+        if(v instanceof Button){
+            temp = (Button) v;
+        }
+        switch (v.getId()){
+            case R.id.button:
+                dialogOk(temp.getText().toString(), new DialogLinstener() {
+                    @Override
+                    public void confirm(Dialog dialog) {
+                        dialog.cancel();
+                    }
+
+                    @Override
+                    public void cancel(Dialog dialog) {
+                        dialog.cancel();
+                    }
+                });
+                break;
+            case R.id.button2:
+                if(x!=null&&splashhandler!=null){
+                    x.postDelayed(splashhandler, 3000);
+                }
+                break;
+            case R.id.button3:
+                showAlert(UDialog.DIALOG_ERROR,temp.getText().toString());
+                break;
+            case R.id.button4:
+                showAlert(UDialog.DIALOG_NORMAL,temp.getText().toString());
+
+                break;
+            case R.id.button5:
+                showAlert(UDialog.DIALOG_SUCCESS,temp.getText().toString());
+
+                break;
+            case R.id.button6:
+                showAlert(UDialog.DIALOG_WARNING,temp.getText().toString());
+
+                break;
+            case R.id.button7:
+                toast(temp.getText().toString(), Toast.LENGTH_SHORT);
+                break;
+            case R.id.button8:
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(x!=null&&splashhandler!=null){
+            x.removeCallbacks(splashhandler);
+        }
     }
 }

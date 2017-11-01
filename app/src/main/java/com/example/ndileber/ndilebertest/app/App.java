ba1341b2d84c85dabc1745241edb65ef567f19a3
wangerdan <297165331@qq.com>
2017-11-01 12:00:36 +0800

    添加多渠道打包

--- app/src/main/java/com/example/ndileber/ndilebertest/app/App.java
+++ app/src/main/java/com/example/ndileber/ndilebertest/app/App.java
@@ -4,6 +4,8 @@ import com.drcosu.ndileber.app.SApplication;
 import com.drcosu.ndileber.tools.annotation.SDefaultFont;
 import com.drcosu.ndileber.tools.annotation.SFontdType;
 import com.drcosu.ndileber.tools.net.RetCallback;
+import com.mcxiaoke.packer.helper.PackerNg;
+
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -16,7 +18,12 @@ import retrofit2.Response;
 public class App extends SApplication {
     @Override
     protected void init() {
-
+// 如果没有使用PackerNg打包添加渠道，默认返回的是""
+// com.mcxiaoke.packer.helper.PackerNg
+        final String channel = PackerNg.getMarket(this);
+// 或者使用 PackerNg.getMarket(Context,defaultValue)
+// 之后就可以使用了，比如友盟可以这样设置
+//        AnalyticsConfig.setChannel(market);
     }
 
     @Override
